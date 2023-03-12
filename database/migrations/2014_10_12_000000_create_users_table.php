25c6076a92c2333cf901a5c6204b7c0f96346b2e
mouheb-ria <mouhebabderrahim@gmail.com>
2023-03-12 16:24:14 +0100

    DATABASE START GENERATED

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,17 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('pseudo')->nullable();
+            $table->date('date_naissance');
+            $table->string('adresse');
             $table->string('email')->unique();
+            $table->biginteger('telephone')->unique();
+            $table->string('ville')->nullable();
+            $table->string('pays')->nullable();
+            $table->string('domaine')->nullable();
+            $table->json('role');
+            $table->string('activite')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
@@ -24,9 +36,11 @@ return new class extends Migration
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
